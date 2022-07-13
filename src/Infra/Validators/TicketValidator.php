a1741616b43beed3fde3689c8e3a189fec4d2e1a
adeonita <adeonita.sousa@gmail.com>
2022-07-12 22:59:37 -0300

    [Ticket] feat: add TicketValidator

--- src/Infra/Validators/TicketValidator.php
+++ src/Infra/Validators/TicketValidator.php
@@ -4,32 +4,43 @@ namespace App\Infra\Validators;
 use App\Domain\Entities\Ticket;
 use App\Domain\Usecases\Session\FindSessionUsecase;
 use App\Domain\Factories\SessionUsecase\FindSessionUsecaseFactory;
+use App\Domain\Factories\UserUsecase\FindUserUsecaseFactory;
+use App\Domain\Factories\RoomUsecase\FindRoomUsecaseFactory;
 
 class TicketValidator
 {
-
   public function validateCreate() {
-    $isStudent = $_POST['isStudent'];
+    $hasIsStudent = isset($_POST['isStudent']);
     $userId = $_POST['userId'];
     $sessionId = $_POST['sessionId'];
 
-    $requiredFields = $isStudent && $sessionId && $userId;
-
-    //chamar o useCase da sessão para pegar a data
-    //calcular o valor do ingresso checando se é fim de semana
-    // e se é meia entrada
-
+    $requiredFields = $hasIsStudent && $sessionId && $userId;
 
     if ($requiredFields) {
-      $sessionFactory = new FindSessionUsecaseFactory();
-      $sessionUsecase = $sessionFactory->create();
-      $sessionUsecase->execute();
+      $isStudent = (int) $_POST['isStudent'];
 
-      echo 'ok'; exit;
-        // return new Ticket(
-        //     null,
-        //     
-        // );
+      $sessionUsecaseFactory = new FindSessionUsecaseFactory();
+      $sessionUsecase = $sessionUsecaseFactory->create();
+      $session = $sessionUsecase->execute($sessionId);
+
+      $roomId = $session->roomId;
+      $roomUsecaseFactory = new FindRoomUsecaseFactory();
+      $roomUsecase = $roomUsecaseFactory->create();
+      $room = $roomUsecase->execute($roomId);
+
+      $userUsecaseFactory = new FindUserUsecaseFactory();
+      $userUsecase = $userUsecaseFactory->create();
+      $user = $userUsecase->execute($userId);
+
+      return new Ticket(
+        null,
+        $session->dateTime,
+        $user->id,
+        $isStudent,
+        $sessionId,
+        $roomId,
+        $room->isThreeDimentions
+      );
     }
             
     throw new \Exception("Validation error: ". count($_POST) .implode(",", $_POST), 422);
